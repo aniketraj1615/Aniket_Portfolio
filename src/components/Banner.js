64f4ab2b83715235ce4a496df3455d688c9eb453
aniketraj1615 <aniketraj1615@gmail.com>
2024-05-11 11:02:58 +0530

    modification

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header.gif";
+import headerImg from "../assets/img/header2.gif";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
