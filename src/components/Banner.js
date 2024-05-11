b0fa9a222b254f98f0cdc3e3a8fa40306bd66bb8
aniketraj1615 <aniketraj1615@gmail.com>
2024-05-11 11:37:20 +0530

    modification 2

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -75,12 +75,12 @@ export const Banner = () => {
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <TrackVisibility>
+            {/* <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
                   <img src={headerImg} alt="Header Img"/>
                 </div>}
-            </TrackVisibility>
+            </TrackVisibility> */}
           </Col>
         </Row>
       </Container>
